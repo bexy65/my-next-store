d06c93613e959514943bc988123d5eba2b78add8
bexy65 <naimertan65@gmail.com>
2024-04-04 13:33:23 +0300

    add navbar server component with fetch data

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
 import React from "react";
+const url: string = "https://fakestoreapi.com/products/categories";
 
-const Navbar = () => {
-  return <nav>Navbar</nav>;
+async function fetchCategories(url: string): Promise<any> {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data as any[];
+}
+
+function clearString(str: string) {
+  return str.replace(/[^\w]/g, "") as string;
+}
+
+const Navbar = async () => {
+  const productCategories: string[] = await fetchCategories(url);
+
+  return (
+    <ul className="flex flex-col md:flex-row p-2 justify-center text-center">
+      {productCategories?.map((e: string, index: number) => (
+        <button key={index} className="p-1 text-start">
+          <Link href={`/${clearString(e)}`}>{e}</Link>
+        </button>
+      ))}
+    </ul>
+  );
 };
 
 export default Navbar;
