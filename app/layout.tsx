93fc6631b1d64193e9acbdea5de3bf1fa0bfe08d
bexy65 <naimertan65@gmail.com>
2024-04-06 12:31:49 +0300

    naming on imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/NavbarCategories";
-import NavbarCategories from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import NavbarCategories from "./components/NavbarCategories";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavbarCategories>
-          <Navbar />
-        </NavbarCategories>
+        <Navbar>
+          <NavbarCategories />
+        </Navbar>
 
         {children}
       </body>
