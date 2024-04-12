15533310dcaa7ae5347d761389f8d0a2fb3a47d4
bexy65 <naimertan65@gmail.com>
2024-04-12 13:33:17 +0300

    refactor the code and cleanup

--- app/boots/[id]/page.tsx
+++ app/boots/[id]/page.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import PocketBase from "pocketbase";
-const db = new PocketBase("http://127.0.0.1:8090");
-
-// const getBoots = async (id: string) => {
-//   const result = await db.collection("products").getList(1, 20, {
-//     filter: `id === ${id}`,
-//   });
-// };
+import ProductPage from "@/app/components/ProductPage";
 
 const getBoot = async (id: string) => {
   const res = await fetch(
@@ -17,26 +10,13 @@ const getBoot = async (id: string) => {
   return boot;
 };
 
-const ProductPage = async ({ params }: any) => {
+const Product = async ({ params }: any) => {
   const boot = await getBoot(params.id);
   return (
-    <div className="product-page flex flex-col md:flex-row text-center w-full p-4 rounded-xl">
-      <div className="h-1/2 md:w-1/2 md:h-full flex flex-col justify-center mb-4">
-        <h1 className="my-2">{boot.name}</h1>
-        <img src={`${boot.image}`} alt="Image" className="h-3/4" />
-      </div>
-      <div className="h-1/2 md:w-1/2 md:h-full flex flex-col justify-around content-center text-center mg-4">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-          necessitatibus perspiciatis itaque cum rem optio neque et excepturi
-          ullam eos placeat saepe officiis nulla, eveniet porro facere commodi,
-          quibusdam recusandae.
-        </p>
-        <p className="text-2xl">{boot.price}$</p>
-        <button>Add to card</button>
-      </div>
-    </div>
+    <>
+      <ProductPage name={boot.name} price={boot.price} image={boot.image} />
+    </>
   );
 };
 
-export default ProductPage;
+export default Product;
